5103b426a13220017cf5a7a7bc7b40ebd9079828
Jason <jrosasrjf@gmail.com>
2021-07-10 00:44:52 -0500

    started to populate the project section. Portfolio should be ready soon :)

--- src/pages/Portfolio/portfolio.jsx
+++ src/pages/Portfolio/portfolio.jsx
@@ -1,13 +1,24 @@
 import "./portfolio.css";
-
+import picturePWGenerator from "./passwordGenerator.JPG"
 
 
 const Portfolio = () => {
   return (
     <div id = "projects">
-      <h1> the project section </h1>
+        <h1> Web Development Project </h1>
+
+        <article>
+            <div>
+                <h3>Password Generator</h3>
+                <img class = "screenCaptures" src = {picturePWGenerator} alt = "screenshort" />
+                
+
+            </div>
+        
+        
 
 
+        </article>
 
 
     </div>
